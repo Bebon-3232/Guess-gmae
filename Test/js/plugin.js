f41fc7026d5e85976f21fef1c02cb8e8270fae24
Bebon-3232 <78825016+Bebon-3232@users.noreply.github.com>
2021-02-17 14:31:00 +0200

    Add files via upload

---
+++ Test/js/plugin.js
@@ -0,0 +1,90 @@
+/* jslint plusplus: true, evil: true */
+/* global console, alert, prompt */
+
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+var specialCharcter = document.getElementById("specialCharcter");
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+  // Validate special charter 
+   var specialcase = /[!@#$%^&*]/g;
+   if(myInput.value.match(specialcase)) {
+    specialCharcter.classList.remove("invalid");
+    specialCharcter.classList.add("valid");
+  } else {
+    specialCharcter.classList.remove("valid");
+    specialCharcter.classList.add("invalid");
+  }
+}
+
+function Validate() {
+var password = document.getElementById("psw").value;
+var confirmPassword = document.getElementById("psw-repeat").value;
+if (password != confirmPassword) {
+alert("You first Passwords is not similar with 2nd password. Please enter same password in both");
+return false;
+}
+return true;
+}
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(document).ready(function(){
+  $("#Login").click(function(){
+  $("#Login").slideToggle("slow");
+  });
+});
